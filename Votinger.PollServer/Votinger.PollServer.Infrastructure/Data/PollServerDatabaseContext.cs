cedb8d4933bcf5c763e24d3e7d43271cfc648769
SeanWoo <kokarevvadim2002@gmail.com>
2021-10-16 01:01:57 +0400

    added base template for poll server

---
+++ Votinger.PollServer/Votinger.PollServer.Infrastructure/Data/PollServerDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votinger.PollServer.Infrastructure.Data
+{
+    public class PollServerDatabaseContext : DbContext
+    {
+        public PollServerDatabaseContext(DbContextOptions<PollServerDatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Votinger.PollServer/Votinger.PollServer.Web/Extensions/IoCExtensions/DatabaseExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Votinger.PollServer.Infrastructure.Data;
+
+namespace Votinger.PollServer.Web.Extensions.IoCExtensions
+{
+    public static class DatabaseExtension
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<PollServerDatabaseContext>(options => 
+                options.UseMySql(
+                    connectString, 
+                    ServerVersion.AutoDetect(connectString)
+                )
+            );
+
+            return services;
+        }
+    }
+}
